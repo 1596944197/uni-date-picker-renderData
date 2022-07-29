b4a344dec996d364a987e29681330c28120e9f40
梅一一 <1596944197@qq.com>
2022-07-29 11:47:43 +0800

    代码更新，增加示例

--- index.ts
+++ index.ts
@@ -1,16 +1,68 @@
 type ColumnDataType = { text: string; value: string; children: ColumnDataType }[];
 
-function renderColumnData<T extends AnyArray>(list: T, key:string): ColumnDataType {
+/**
+ * @param {
+ * list为源数组
+ * key为源数组每个对象中的期望展示为text的值
+ * value为源数组每个对象中期望回调的值
+ * childListKey为递归对象的键值
+ * }
+ */
+
+export function renderColumnData<T extends AnyArray>({
+  list,
+  key,
+  value,
+  childListKey,
+}:{
+  list: T,
+  key:string,
+  value:any,
+  childListKey: string,
+}): ColumnDataType {
   if (!list) return [];
   return list.map?.((item) => ({
-    text: item.name,
-    value: item.id,
-    children: renderColumnData(item, key),
+    text: item[key],
+    value: item[value],
+    children: renderColumnData({
+      list: item[childListKey], key, value, childListKey,
+    }),
   }));
 }
 
-export {
-  renderColumnData,
-};
+// # 使用示例
+const test = [{
+  name: '一年级',
+  id: '1-0',
+  children: [
+    {
+      name: '1.1班',
+      id: '1-1',
+      children: [
+        {
+          name: '草拟大额',
+          id: 'bbb',
+        },
+      ],
+    },
+    {
+      name: '1.2班',
+      id: '1-2',
+    },
+  ],
+},
+{
+  name: '二年级',
+  id: '2-0',
+},
+{
+  name: '三年级',
+  id: '3-0',
+}];
 
-export default {};
+renderColumnData({
+  list: test,
+  key: 'name',
+  value: 'id',
+  childListKey: 'children',
+});
